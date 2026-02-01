93c0c585150ff67b0ce50e303880c366ae170f61
5uperb0y <https://github.com/5uperb0y>
2026-02-01 11:43:05 +0800

    style: unify whitespace, quotes, and indentation with ruff

--- shortener/models.py
+++ shortener/models.py
@@ -7,18 +7,25 @@ class Link(models.Model):
     """
     Map urls and their slugs
     """
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='links')
     url = models.URLField(max_length=2048, help_text='An url to be shorten')
-    slug = models.CharField(max_length=7, unique=True, help_text='A random 7-character code (i.e. [a-zA-Z0-9])')
+    slug = models.CharField(
+        max_length=7,
+        unique=True,
+        help_text='A random 7-character code (i.e. [a-zA-Z0-9])',
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-created_at']
 
+
 class Click(models.Model):
     """
     Visitors' ip for each slug
     """
+
     link = models.ForeignKey(Link, on_delete=models.CASCADE, related_name='clicks')
     ip = models.GenericIPAddressField()
     clicked_at = models.DateTimeField()
